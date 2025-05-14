15bf66e509b57375e6462dd09ba90a3127075ca0
Sembatya2020 <ivansmbt@gmail.com>
2025-05-14 16:56:45 +0300

    updating

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,4 +4,9 @@ const router = express.Router();
 const contactsController = require('../controllers/contacts');
 router.get('/', contactsController.getAll);
 router.get('/:id', contactsController.getSingle);
+
+router.post('/', contactsController.createUser);
+router.put('/:id', contactsController.updateUser);
+router.delete('/:id', contactsController.deleteUser);
+
 module.exports = router;
